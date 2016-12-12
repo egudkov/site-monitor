6deece4537c792da3d8daa53756fdee5394c3c51
Evgeny Gudkov <gudkoveyu@gmail.com>
2016-12-12 09:54:00 +0300

    US-8: Added 'add_website' and 'show_websites' functions, html, css.

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import requests
 import os
 import sqlite3
-from flask import Flask, render_template, g
+from flask import Flask, render_template, g, request, redirect, flash, \
+    url_for
 app = Flask(__name__)
 app.config.from_object(__name__)
 
@@ -21,11 +22,13 @@ websites = {
     'http://httpbin.org/status/500': 'OK'
     }
 
+
 def connect_db():
     """Connects to the specific database."""
     rv = sqlite3.connect(app.config['DATABASE'])
     rv.row_factory = sqlite3.Row
     return rv
+
 
 def init_db():
     db = get_db()
@@ -33,11 +36,13 @@ def init_db():
         db.cursor().executescript(f.read())
     db.commit()
 
+
 @app.cli.command('initdb')
 def initdb_command():
     """Initializes the database."""
     init_db()
     print 'Initialized the database.'
+
 
 def get_db():
     """Opens a new database connection if there is none yet for the
@@ -47,6 +52,7 @@ def get_db():
         g.sqlite_db = connect_db()
     return g.sqlite_db
 
+
 @app.teardown_appcontext
 def close_db(error):
     """Closes the database again at the end of the request."""
@@ -54,14 +60,32 @@ def close_db(error):
         g.sqlite_db.close()
 
 
+@app.route('/')
+def show_websites():
+    db = get_db()
+    cur = db.execute('select url, status from websites order by id desc')
+    websites = cur.fetchall()
+    return render_template('show_websites.html', websites=websites)
+
+
+@app.route('/add', methods=['POST'])
+def add_website():
+    db = get_db()
+    db.execute('insert into websites (url) values (?)',
+        [request.form['url']])
+    db.commit()
+    flash('New website was successfully added')
+    return redirect(url_for('show_websites'))
+
+
 # route() decorator tells Flask
 # what URL should trigger our function
-@app.route('/')
-def get_status():
-    for url in websites:
-        r = requests.get(url)
-        if r.status_code == requests.codes.ok:
-            websites[url] = 'OK'
-        else:
-            websites[url] = 'NOT_OK'
-    return render_template('index.html', websites=websites)
+# @app.route('/')
+# def get_status():
+#     for url in websites:
+#         r = requests.get(url)
+#         if r.status_code == requests.codes.ok:
+#             websites[url] = 'OK'
+#         else:
+#             websites[url] = 'NOT_OK'
+#     return render_template('index.html', websites=websites)
--- schema.sql
+++ schema.sql
@@ -2,5 +2,5 @@ drop table if exists websites;
 create table websites (
   id integer primary key autoincrement,
   url text not null,
-  status text not null
+  status text default 'Pending...'
 );
